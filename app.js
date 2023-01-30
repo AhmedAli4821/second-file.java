ed76d72d237b3bce0c36dad213d3aa16f210e6a7
AhmedAli4621 <ahmadali4821@gmail.com>
2023-01-30 09:34:01 -0800

    second file java

---
+++ app.js
@@ -0,0 +1,24 @@
+var mName = "Ahmed Ali"
+alert(mName)
+
+var a = "Hellow World"
+
+alert (a)
+
+var b = "Certified Mobile Application Development"
+
+alert (b)
+
+var c = "Pizza\nPizz\nPiz\nPi\nP"
+
+alert (c)
+
+var d = "My email address is example@example.com"
+
+alert (d)
+
+document.write("Yah! I can write HTML content Through JavaScript")
+
+var e = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+
+alert (e)
